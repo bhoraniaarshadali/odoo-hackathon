28c29e8490cbdbf4809f3afd80e6df70f5bbf6a1
hardik <hardikkasli123@gmail.com>
2025-07-12 12:46:19 +0530

    Initial commit

---
+++ core/models.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    is_public = models.BooleanField(default=True)
+    availability = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_offered = models.BooleanField(default=True)  # True = offered, False = wanted
+
+    def __str__(self):
+        return f"{self.name} ({'Offer' if self.is_offered else 'Want'})"
+
+class SwapRequest(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_requests')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_requests')
+    skill_requested = models.ForeignKey(Skill, on_delete=models.CASCADE, related_name='requested')
+    skill_offered = models.ForeignKey(Skill, on_delete=models.CASCADE, related_name='offered')
+    status = models.CharField(max_length=10, choices=[('pending', 'Pending'), ('accepted', 'Accepted'), ('rejected', 'Rejected')], default='pending')
+
+
+class Feedback(models.Model):
+    swap = models.ForeignKey(SwapRequest, on_delete=models.CASCADE)
+    from_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='feedbacks_from', null=True, blank=True)
+    comments = models.TextField()
+    rating = models.IntegerField()
